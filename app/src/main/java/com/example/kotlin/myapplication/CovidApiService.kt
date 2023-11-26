ff20ee0dd2e16de62c1ca08ee448106ea340b56e
leshcsr <a01708987@tec.mx>
2023-11-26 14:06:00 -0600

    feat: Add CovidDataResponse

--- app/src/main/java/com/example/kotlin/myapplication/CovidApiService.kt
+++ app/src/main/java/com/example/kotlin/myapplication/CovidApiService.kt
@@ -2,6 +2,7 @@ package com.example.kotlin.myapplication
 
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 /**
  * Interfaz que define las operaciones disponibles en el servicio web para obtener datos de COVID.
@@ -14,6 +15,11 @@ interface CovidApiService {
      * @param apiKey Clave de la API para autenticación.
      * @return Lista de objetos [CovidData] que representan los datos de COVID.
      */
-    @GET("v1/covid")
-    suspend fun getCovidData(@Header("X-API-KEY") apiKey: String): List<CovidData>
+    @GET("v1/covid19")
+    suspend fun getCovidData(
+        @Query("country") country: String,
+        @Query("region") region: String,
+        @Header("X-Api-Key") apiKey: String = "wLVPN1zV08lJYF7uXqgyPw==zVwp6TlVcAO1NLUf",
+    ): CovidDataResponse
+
 }
---
+++ app/src/main/java/com/example/kotlin/myapplication/CovidDataResponse.kt
@@ -0,0 +1,3 @@
+package com.example.kotlin.myapplication
+
+data class CovidDataResponse()
