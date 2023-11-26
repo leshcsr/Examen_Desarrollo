c30433994b52c8496011a9d1e332bd42b335d952
leshcsr <a01708987@tec.mx>
2023-11-26 14:24:42 -0600

    feat: Add add structure to CovidDataResponse

--- app/src/main/java/com/example/kotlin/myapplication/CovidDataResponse.kt
+++ app/src/main/java/com/example/kotlin/myapplication/CovidDataResponse.kt
@@ -1,3 +1,12 @@
 package com.example.kotlin.myapplication
+import com.google.gson.annotations.SerializedName
+data class CovidDataResponse(
+    val country: String,
+    val region: String,
+    val cases: Map<String, CaseData>
+)
 
-data class CovidDataResponse()
+data class CaseData(
+    val total: Int,
+    val new: Int
+)
